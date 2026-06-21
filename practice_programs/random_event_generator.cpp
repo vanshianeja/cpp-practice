b9f8546e809c49a0508bd110f2236c6585f27033
vanshianeja <vanshianeja512@gmail.com>
2026-06-22 04:30:03 +0530

    add random event generator

---
+++ practice_programs/random_event_generator.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <ctime>
+
+int main() {
+
+    srand(time(0));
+
+    int randNum = (rand() % 5) + 1;
+
+    switch(randNum) {
+        case 1:
+            std::cout << "You win a bumper sticker!\n";
+            break;
+        case 2:
+            std::cout << "You win a free lunch!\n";
+            break;
+        case 3:
+            std::cout << "You win a t-shirt!\n";
+            break;
+        case 4: 
+            std::cout << "You win a git card!\n";
+            break;
+        case 5:
+            std::cout << "You win a concert ticket!\n";
+            break;
+    }
+
+    return 0;
+}
